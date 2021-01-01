079467ce2e8cb174e1ff2730619ff459c6ef12d2
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2021-01-01 17:10:37 +0100

    get rid of the filter

--- react-app/src/components/Stack/tree/index.js
+++ react-app/src/components/Stack/tree/index.js
@@ -9,7 +9,6 @@ import st from '../../../data/stack'
 export default function FileSystemNavigator(props) {
     const [stacks, setStacks] = useState(st);
     const [input, setInput] = useState({});
-    const [filter, setFilter] = useState("");
 
     const config = open => ({
         from: {height: 0, opacity: 0, transform: 'translate3d(20px,0,0)'},
@@ -32,18 +31,6 @@ export default function FileSystemNavigator(props) {
         setInput({value: event.target.value});
     };
 
-    const handleFilter = function (event) {
-        setFilter(event.target.value);
-    };
-
-    function filterTree() {
-        setStacks({
-            ...stacks,
-            stackTraceLines: stacks.stackTraceLines.filter(line => line.packageName.includes(filter))
-        });
-
-    }
-
     return <span>
     <div>
         <div className="d-md-flex h-md-50 align-items-center scrollable">
@@ -55,16 +42,7 @@ export default function FileSystemNavigator(props) {
                 </div>
             </div>
             <div className="col-md-9 p-0  h-md-100 scrollable dark-background">
-                <div className="filter col-md-6">
-                    <div className={"input-group mb-3"}>
-                    <input type="text" placeholder="filter by package / or class name"
-                           className="form-control"
-                           onChange={handleFilter}/>
-                    <div className={"input-group-prepend"}>
-                        <button onClick={filterTree} className={"btn btn-primary"}> filter </button>
-                    </div>
-                </div>
-            </div>
+
             <div className="d-md-flex p-4">
                 <Tree open visible content="Stack trace" springConfig={config}>
                     <StackTrace trace={stacks}/>
