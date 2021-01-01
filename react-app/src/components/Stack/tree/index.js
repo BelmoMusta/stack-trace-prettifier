11a729f9e1752d421358cdeeff7fbb8e57339d60
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2021-01-01 16:23:28 +0100

    stable themed version

--- react-app/src/components/Stack/tree/index.js
+++ react-app/src/components/Stack/tree/index.js
@@ -45,33 +45,34 @@ export default function FileSystemNavigator(props) {
     }
 
     return <span>
-        Convert a java stack trace into a readable tree view
-              <div>
-                    <div className="d-md-flex h-md-50 align-items-center scrollable">
-                    <div className="col-md-3 p-0 bg-indigo h-md-100">
-                        <div>
-                                     <textarea
-                                         value={input.value}
-                                         onChange={handleChange}
-                                         rows={20} id="stackTrace" className={"textarea"}/>
-                                     <button className={"pull-right"} onClick={callApi}> Prettify </button>
-
-                        </div>
-                    </div>
-                <div className="col-md-9 p-0  h-md-100 scrollable dark-background">
-                    <div className={"filter"}>
-                      <input type="text" placeholder="filter by package / or class name" className="form-control"
-                             onChange={handleFilter}
-                      />
-                      <button onClick={filterTree}> filter </button>
-                    </div>
-                    <div className="d-md-flex  p-4">
-                        <Tree open={false} canHide visible content="Stack trace" springConfig={config}>
-                            <StackTrace trace={stacks}/>
-                        </Tree>
+    <div>
+        <div className="d-md-flex h-md-50 align-items-center scrollable">
+            <div className="col-md-3 p-0 bg-indigo h-md-100">
+                <div>
+                    <textarea value={input.value} onChange={handleChange} rows={20}
+                              id="stackTrace" className={"textarea"}/>
+                        <button className={"prettify-button"} onClick={callApi}>Prettify</button>
+                </div>
+            </div>
+            <div className="col-md-9 p-0  h-md-100 scrollable dark-background">
+                <div className="filter col-md-6">
+                    <div className={"input-group mb-3"}>
+                    <input type="text" placeholder="filter by package / or class name"
+                           className="form-control"
+                           onChange={handleFilter}/>
+                    <div className={"input-group-prepend"}>
+                        <button onClick={filterTree} className={"btn btn-primary"}> filter </button>
                     </div>
                 </div>
-          </div>
-              </div>
-    </span>
+            </div>
+            <div className="d-md-flex p-4">
+                <Tree open visible content="Stack trace" springConfig={config}>
+                    <StackTrace trace={stacks}/>
+                </Tree>
+            </div>
+        </div>
+    </div>
+    </div>
+</span>
+
 }
