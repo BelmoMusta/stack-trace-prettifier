3877717b3ec0a62c1628051a9b60853ba47dcb29
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2021-01-01 04:35:28 +0100

    use dark them

--- react-app/src/components/Stack/tree/index.js
+++ react-app/src/components/Stack/tree/index.js
@@ -4,7 +4,6 @@ import Tree from "react-animated-tree";
 import StackTrace from "./stacktrace";
 import {post} from '../../../restService/restApiCaller';
 import st from '../../../data/stack'
-import {dark} from "@material-ui/core/styles/createPalette";
 
 
 export default function FileSystemNavigator(props) {
@@ -38,34 +37,35 @@ export default function FileSystemNavigator(props) {
     };
 
     function filterTree() {
-        setStacks({...stacks, stackTraceLines: stacks.stackTraceLines.filter(line => line.packageName.includes(filter))});
+        setStacks({
+            ...stacks,
+            stackTraceLines: stacks.stackTraceLines.filter(line => line.packageName.includes(filter))
+        });
 
     }
 
-    return  <span >
+    return <span>
         Convert a java stack trace into a readable tree view
               <div>
-                    <div className="d-md-flex h-md-100 align-items-center scrollable">
-                    <div className="col-md-6 p-0 bg-indigo h-md-100 hidden">
-                        <div
-                            className="text-white d-md-flex_ align-items-center_ h-100_ p-3 text-center_ justify-content-center_">
-                            <div className="pt-5 pb-5">
+                    <div className="d-md-flex h-md-50 align-items-center scrollable">
+                    <div className="col-md-3 p-0 bg-indigo h-md-100">
+                        <div>
                                      <textarea
                                          value={input.value}
                                          onChange={handleChange}
                                          rows={20} id="stackTrace" className={"textarea"}/>
                                      <button className={"pull-right"} onClick={callApi}> Prettify </button>
-                            </div>
+
                         </div>
                     </div>
-                <div className="scol-md-6 p-0  hf-md-100 scrollable" className={"dark-background"}>
+                <div className="col-md-9 p-0  h-md-100 scrollable dark-background">
                     <div className={"filter"}>
                       <input type="text" placeholder="filter by package / or class name" className="form-control"
-                      onChange={handleFilter}
+                             onChange={handleFilter}
                       />
                       <button onClick={filterTree}> filter </button>
                     </div>
-                    <div className="ds-md-flex_ hs-md-100 ps-4">
+                    <div className="d-md-flex  p-4">
                         <Tree open={false} canHide visible content="Stack trace" springConfig={config}>
                             <StackTrace trace={stacks}/>
                         </Tree>
--- react-app/src/components/Stack/tree/stacktrace.js
+++ react-app/src/components/Stack/tree/stacktrace.js
@@ -10,19 +10,13 @@ function StackTrace(props) {
             {trace.prolog && <Prolog prolog={trace.prolog}/>}
             {trace.stackTraceLines &&
 
-            <Tree style={{ color: "#f83737" }} content={<span className={"package"}>{trace.stackTraceLines.length + "  IMPORTANT LINES"}
+            <Tree style={{ color: "#f83737" }}
+                  content={<span className={"package"}>{trace.stackTraceLines.length + " LINES"}
             </span>}>
                 {(trace.stackTraceLines.map(l =>
-                    (trace.stackTraceLines.map(l => <Line line={l}/>))
-                ))}</Tree>}}
-
-            {trace.importantLines &&
-            <Tree content={<span className={"package"}>{trace.importantLines.length + "  IMPORTANT LINES"}
-            </span>}>
-
-                {(trace.importantLines.map(l =>
-                    <span>{<Line line={l}/>}</span>
+                    <Line line={l}/>
                 ))}</Tree>}
+
 
             {trace.causedBy &&
             <Tree content={<span className={"package"}>CAUSED BY</span>}> <StackTrace trace={trace.causedBy}/></Tree>}
