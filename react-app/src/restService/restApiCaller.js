3c5d08929cd485b3d85b22e2a88317c64292db48
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2021-01-07 00:10:51 +0100

    all

--- react-app/src/restService/restApiCaller.js
+++ react-app/src/restService/restApiCaller.js
@@ -3,11 +3,13 @@ export function post(input, callback) {
         method: 'POST',
         body: JSON.stringify(input),
         headers: {
-            'Content-Type': 'application/json; charset=utf-8'
+            'Content-Type': 'application/json; charset=utf-8',
+            'Access-Control-Allow-Origin' :'*',
+            'Access-Control-Allow-Headers': 'X-Requested-With'
         }
     };
 
-   return  fetch('http://51.222.106.8:8080/prettify', requestOptions)
+   return  fetch('https://51.222.106.8:8080/prettify', requestOptions)
        .then(res => res.json())
        .then(data => callback(data))
 
